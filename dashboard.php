7019ee4efa1bb6415b543ac96df5253b99b75534
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2020-12-16 11:59:27 +0600

    material list updated

--- dashboard.php
+++ dashboard.php
@@ -24,20 +24,20 @@ $(document).ready(function() {
         <!-- Icon Cards-->
 		<?php if($_SESSION['logged']['user_type'] !== 'whm') {?>
         <div class="row">
-		  <div class="col-xl-3 col-sm-6 mb-3">
+          <div class="col-xl-3 col-sm-6 mb-3">
             <div class="card text-white bg-primary o-hidden h-100">
               <div class="card-body">
                 <div class="card-body-icon">
                   <i class="fas fa-fw fa-comments"></i>
                 </div>
 				<?php
-				$sqlsup	=	"SELECT * FROM suppliers";
-				$resultsup = mysqli_query($conn, $sqlsup);
-				$totalSupplier = mysqli_num_rows($resultsup);
+				$sqlpck	=	"SELECT * FROM `equipments` where `type_id`='47'";
+				$resultpck = mysqli_query($conn, $sqlpck);
+				$totalPackage = mysqli_num_rows($resultpck);
 				?>
-                <div class="mr-5"><?php echo $totalSupplier; ?> Total RTG</div>
+                <div class="mr-5"><?php echo $totalPackage; ?> Total RTG</div>
               </div>
-              <a class="card-footer text-white clearfix small z-1" href="#">
+              <a class="card-footer text-white clearfix small z-1" href="equipment_entry.php">
                 <span class="float-left">View Details</span>
                 <span class="float-right">
                   <i class="fas fa-angle-right"></i>
@@ -52,34 +52,13 @@ $(document).ready(function() {
                   <i class="fas fa-fw fa-comments"></i>
                 </div>
 				<?php
-				$sqlwar	=	"SELECT * FROM inv_warehosueinfo";
-				$resultwar = mysqli_query($conn, $sqlwar);
-				$totalWarehouse = mysqli_num_rows($resultwar);
-				?>
-                <div class="mr-5"><?php echo $totalWarehouse; ?> Total RST</div>
-              </div>
-              <a class="card-footer text-white clearfix small z-1" href="#">
-                <span class="float-left">View Details</span>
-                <span class="float-right">
-                  <i class="fas fa-angle-right"></i>
-                </span>
-              </a>
-            </div>
-          </div>
-          <div class="col-xl-3 col-sm-6 mb-3">
-            <div class="card text-white bg-primary o-hidden h-100">
-              <div class="card-body">
-                <div class="card-body-icon">
-                  <i class="fas fa-fw fa-comments"></i>
-                </div>
-				<?php
-				$sqlpck	=	"SELECT * FROM packages";
+				$sqlpck	=	"SELECT * FROM `equipments` where `type_id`='46'";
 				$resultpck = mysqli_query($conn, $sqlpck);
 				$totalPackage = mysqli_num_rows($resultpck);
 				?>
                 <div class="mr-5"><?php echo $totalPackage; ?> Total QGC</div>
               </div>
-              <a class="card-footer text-white clearfix small z-1" href="#">
+              <a class="card-footer text-white clearfix small z-1" href="equipment_entry.php">
                 <span class="float-left">View Details</span>
                 <span class="float-right">
                   <i class="fas fa-angle-right"></i>
@@ -94,13 +73,34 @@ $(document).ready(function() {
                   <i class="fas fa-fw fa-comments"></i>
                 </div>
 				<?php
-				$sqlbld	=	"SELECT * FROM buildings";
-				$resultbld = mysqli_query($conn, $sqlbld);
-				$totalBuilding = mysqli_num_rows($resultbld);
+				$sqlpck	=	"SELECT * FROM `equipments` where `type_id`='51'";
+				$resultpck = mysqli_query($conn, $sqlpck);
+				$totalPackage = mysqli_num_rows($resultpck);
 				?>
-                <div class="mr-5"><?php echo $totalBuilding; ?> Total Trailer</div>
+                <div class="mr-5"><?php echo $totalPackage; ?> Total TRAILER</div>
               </div>
-              <a class="card-footer text-white clearfix small z-1" href="#">
+              <a class="card-footer text-white clearfix small z-1" href="equipment_entry.php">
+                <span class="float-left">View Details</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-primary o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-comments"></i>
+                </div>
+				<?php
+				$sqlpck	=	"SELECT * FROM `equipments` where `type_id`='49'";
+				$resultpck = mysqli_query($conn, $sqlpck);
+				$totalPackage = mysqli_num_rows($resultpck);
+				?>
+                <div class="mr-5"><?php echo $totalPackage; ?> Total FLT</div>
+              </div>
+              <a class="card-footer text-white clearfix small z-1" href="equipment_entry.php">
                 <span class="float-left">View Details</span>
                 <span class="float-right">
                   <i class="fas fa-angle-right"></i>
@@ -194,109 +194,7 @@ $(document).ready(function() {
         </div>
 		
 <?php } ?>
-<div class="row">
-	<div class="col-xl-12 col-sm-12 mb-3"><!--here your code will go-->
-			<script>
-			window.onload = function () {
 
-			var chart = new CanvasJS.Chart("chartContainer", {
-				animationEnabled: true,
-				title:{
-					text: "Top items stock at a glance"
-				},	
-				axisY: {
-					title: "Items in Qty",
-					titleFontColor: "#4F81BC",
-					lineColor: "#4F81BC",
-					labelFontColor: "#4F81BC",
-					tickColor: "#4F81BC"
-				},
-				axisY2: {
-					title: "Items in Qty",
-					titleFontColor: "#C0504E",
-					lineColor: "#C0504E",
-					labelFontColor: "#C0504E",
-					tickColor: "#C0504E"
-				},	
-				toolTip: {
-					shared: true
-				},
-				legend: {
-					cursor:"pointer",
-					itemclick: toggleDataSeries
-				},
-				data: [
-				<?php
-				$projectsData = getwarehouseinfo('inv_warehosueinfo');
-				if (isset($projectsData) && !empty($projectsData)) {
-					foreach ($projectsData as $data) {
-				?>
-				
-				
-				{
-					type: "column",	
-					name: "<?php echo $data['name']; ?>",
-					legendText: "<?php echo $data['name']; ?>",
-					axisYType: "secondary",
-					showInLegend: true,
-					dataPoints:[
-					
-					<?php 
-					$sql	=	"SELECT * FROM inv_material WHERE material_id = 13";
-						$result = mysqli_query($conn, $sql);
-						while($row=mysqli_fetch_array($result))
-						{
-							$warehouse 		= $data['id'];
-							$to_date 		= date('Y-m-d');
-							$mb_materialid 	= $row['material_id_code'];
-							
-							$sqlinqty = "SELECT SUM(`mbin_qty`) AS totalin FROM `inv_materialbalance` WHERE warehouse_id = $warehouse AND `mb_materialid` = '$mb_materialid' AND mb_date <= '$to_date'";
-							$resultinqty = mysqli_query($conn, $sqlinqty);
-							$rowinqty = mysqli_fetch_object($resultinqty) ;
-							
-							
-							$sqloutqty = "SELECT SUM(`mbout_qty`) AS totalout FROM `inv_materialbalance` WHERE warehouse_id = $warehouse AND `mb_materialid` = '$mb_materialid' AND mb_date <= '$to_date'";
-							$resultoutqty = mysqli_query($conn, $sqloutqty);
-							$rowoutqty = mysqli_fetch_object($resultoutqty) ;
-							
-							//echo $rowinqty->totalin -$rowoutqty->totalout;
-							
-							$instock = $rowinqty->totalin -$rowoutqty->totalout;
-							
-					?>
-						{ label: "<?php echo $row['material_description']; ?>", y: <?php echo number_format((float)$instock, 2, '.', ''); ?> },
-						
-						<?php } ?>
-					]
-				},
-				
-					<?php
-					}
-				}
-				?>
-				
-				
-				]
-			});
-			chart.render();
-
-			function toggleDataSeries(e) {
-				if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-					e.dataSeries.visible = false;
-				}
-				else {
-					e.dataSeries.visible = true;
-				}
-				chart.render();
-			}
-
-			}
-			</script>
-
-			<div id="chartContainer" style="height: auto; width: 100%;"></div>
-			<script src="js/canvasjs.min.js"></script>
-            <!--here your code will go--></div>
-</div>
 		
 		
 		
