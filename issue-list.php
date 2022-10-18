fa08501c64fc13ea5bca16c250fd7114092c5460
softactor <tanveerqureshee1@gmail.com>
2022-10-18 15:57:45 +0600

    resolving issue list

--- issue-list.php
+++ issue-list.php
@@ -1,121 +1,75 @@
-<?php 
+<?php
 include 'header.php';
 
- ?>
+?>
 <link href="css/dataTables.bootstrap4.min.css" rel="stylesheet">
 <style>
-table th{
-	 font-size:14px;
-}
-table td{
-	 font-size:14px;
-}
+	table th {
+		font-size: 14px;
+	}
+
+	table td {
+		font-size: 14px;
+	}
 </style>
 <div class="container-fluid">
-    <!-- DataTables Example -->
-    <div class="card mb-3">
-        <div class="card-header">
-            <i class="fas fa-table"></i>
-            Material Issue List</div>
-        <div class="card-body">
-            <table id="example" class="table table-striped table-bordered" style="width:100%">
-				<thead>
-					<tr>
-						<th>Issue Date</th>
-						<th>Issue No</th>
-						<th width="30%">Material Name</th>
-						<th>Use in</th>
-						<th>Project</th>
-						<th>Ware House</th>
-					     <th>Action</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					if($_SESSION['logged']['user_type'] == 'whm') {
-						$item_details = getTableDataByTableNameWid('inv_issue', '', 'id');
-					}else{
-						$item_details = getTableDataByTableName('inv_issue', '', 'id');
-					}
-					if (isset($item_details) && !empty($item_details)) {
-						foreach ($item_details as $item) {
-							if($item['approval_status'] == 0){
-							?>
-							<tr style="background-color: #FFC107;max-height:10px;">
-							<?php  }else{ ?>
-							<tr style="background-color: #218838;max-height:10px;">
-							<?php  }?>
-									<td><?php echo date("j M y", strtotime($item['issue_date'])); ?></td>
-								<td><?php echo $item['issue_id']; ?></td>
-								
-								
-								<td><?php 
-								$issue_id = $item['issue_id'];
-								$sql = "select * from `inv_issuedetail` where `issue_id`='$issue_id'";
-								$result = mysqli_query($conn, $sql);
-									for($i=1; $row = mysqli_fetch_array($result); $i++){
-											$dataresult =   getDataRowByTableAndId('inv_material', $row['material_name']);
-											echo (isset($dataresult) && !empty($dataresult) ? $dataresult->material_description : '') . ',' ;
-									}
-								
-								?></td>
-								<?php 
-								$sql = "select * from `inv_issuedetail` where `issue_id`='$issue_id'";
-								
-								
-								$user_categories = mysqli_query($conn, "select `use_in` from `inv_issuedetail` where `issue_id`='$issue_id'");
-								$category_ids = mysqli_fetch_all($user_categories,MYSQLI_NUM);
-								$category_ids_imploded = implode(', ', array_map(function ($entry) {
-								  return $entry['0'];
-								}, $category_ids));
-								
-								?>
-								
-								<td><?php echo $category_ids_imploded; ?></td>
-								<td>
-									<?php 
-									$dataresult =   getDataRowByTableAndId('projects', $item['project_id']);
-									echo (isset($dataresult) && !empty($dataresult) ? $dataresult->name : '');
-									?>
-								</td>
-								<td>
-									<?php 
-									$dataresult =   getDataRowByTableAndId('inv_warehosueinfo', $item['warehouse_id']);
-									echo (isset($dataresult) && !empty($dataresult) ? $dataresult->name : '');
-									?>
-								</td>
-								<td>
-									<span><a class="action-icons c-approve" href="issue-view.php?no=<?php echo $item['issue_id']; ?>" title="View"><i class="fas fa-eye text-success"></i></a></span>
-									<span><a class="action-icons c-delete" href="issue_edit.php?edit_id=<?php echo $item['id']; ?>" title="edit"><i class="fa fa-edit text-info mborder"></i></a></span>
-									<?php if($_SESSION['logged']['user_type'] == 'superAdmin') {?>
-										<span><a class="action-icons c-delete" href="issue_approve.php?issue=<?php echo $item['issue_id']; ?>" title="approve"><i class="fa fa-check text-info mborder"></i></a></span>
-										<?php } ?>
-							<span><a class="action-icons c-delete" href="#" title="delete"><i class="fa fa-trash text-danger"></i></a></span>
-								</td>
-							</tr>
-							<?php
-						}
-					}else{ ?>
-						  <tr>
-							  <td colspan="7">
-									<div class="alert alert-info" role="alert">
-										Sorry, no data found!
-									</div>
-								</td>
-							</tr>  
-					<?php } ?>
-				</tbody>
-			</table>
-        </div>
-    </div>
+	<!-- DataTables Example -->
+	<div class="card mb-3">
+		<div class="card-header">
+			<i class="fas fa-table"></i>
+			Material Issue List
+		</div>
+		<div class="card-body">
+
+			<div class="row">
+				<div class="col-md-12">
+
+					<div class="p-5">
+						<form class="form-inline" id="get_issue_filter_form">
+							<label for="start_date">From Date:</label>
+							<input type="text" class="form-control" placeholder="Enter Start Date" id="start_date" name="start_date" value="<?php echo date('Y-m-d'); ?>">
+							<label for="end_date">End Date:</label>
+							<input type="text" class="form-control" placeholder="Enter End date" id="end_date" name="end_date" value="<?php echo date('Y-m-d'); ?>">
+
+							<button type="button" class="btn btn-primary" onclick="getIssueListDataByDate()">Get Date</button>
+						</form>
+					</div>
+
+
+
+				</div>
+			</div>
+
+			<div id="show_issue_details_data">
+
+
+
+				<?php
+				$date_filter 	=	(object)[
+					'start_date'	=> date('Y-m-d'),
+					'end_date'		=> date('Y-m-d'),
+				];
+				$item_details = get_issue_details_data($date_filter);
+				if (isset($item_details) && !empty($item_details)) {
+
+					show_issue_details_data($item_details);
+				
+				} else { ?>
+					<div class="alert alert-info" role="alert"> Sorry, no data found!</div>
+				<?php } ?>
+
+			</div>
+
+		</div>
+	</div>
 
 </div>
 <!-- /.container-fluid -->
 <?php include 'footer.php' ?>
 <script>
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
+	$(document).ready(function() {
+		$('#example').DataTable();
+	});
 </script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
