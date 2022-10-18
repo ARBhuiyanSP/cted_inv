2f9dac84e7db80ac203e6a5f4db4656db948965f
softactor <tanveerqureshee1@gmail.com>
2022-10-18 15:59:58 +0600

    server change commit

--- js/site_url.js
+++ js/site_url.js
@@ -1,5 +1,4 @@
 var getUrl = window.location;
-console.log('window.location: ' + getUrl);
 var baseurlOrigin = getUrl.origin; //or
-console.log('getUrl.origin: ' + baseurlOrigin);
 var baseUrl = baseurlOrigin+'/cted_inv/';
+console.log('baseUrl: ' + baseUrl);
